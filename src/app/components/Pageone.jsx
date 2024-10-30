82b203cce4e6c42795a70c517596f41882a3d39f
Sameer Yadav <ysameer0026@gmail.com>
2024-10-31 01:21:35 +0530

    first commit

---
+++ src/app/components/Pageone.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Pageone = () => {
+  return (
+    <>
+    <div className='bg-[#4b4b4b] w-60 rounded-lg p-2'>
+        <p className='text-white font-bold text-2xl text-center '>Dashboard</p>
+    </div>
+    <div className='w-60 text-center text-white font-bold  bg-[#4b4b4b] rounded-lg mt-5 p-5 '>
+        <div className=''>
+            <a href="">Page one</a>
+        </div>
+        <div className='mt-5'>
+            <a href="">Page two</a>
+        </div>
+        <div className='mt-5'>
+            <a href="">Page three</a>
+        </div>
+        <div className='mt-5'>
+            <a href="">Page four</a>
+        </div>
+        <div className='mt-5'>
+            <a href="">Page five</a>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Pageone
